6df1d1a7c237c6c49a38d1ae0f8b465f2361527f
James Seppi <james.seppi@gmail.com>
2015-09-08 16:57:59 -0500

    setting up chartist

--- app/public/src/components/ThemeChart.jsx
+++ app/public/src/components/ThemeChart.jsx
@@ -1,12 +1,41 @@
 
+import {PureRenderMixin} from 'react/addons';
 import React from 'react';
+import Spinner from 'react-spinkit';
+import ChartistGraph from 'react-chartist';
 
 export default React.createClass({
+  propTypes: {
+    dataRows: React.PropTypes.array
+  },
+
+  mixins: [PureRenderMixin],
+
   render() {
+    if (this.props.dataRows) {
+      const chartData = {
+        labels: ['MANUFACTURING', 'MINING', 'STEAM-ELECTRIC'],
+        series: [
+          [47000, 5000, 120000]
+        ]
+      };
+
+      const chartOptions = {
+        horizontalBars: true,
+        height: '300px'
+      };
+
+      return (
+        <ChartistGraph
+          data={chartData}
+          options={chartOptions}
+          type={'Bar'}
+        />
+      );
+    }
+
     return (
-      <div>
-        THEME CHART
-      </div>
+      <Spinner spinnerName="double-bounce" />
     );
   }
 });
