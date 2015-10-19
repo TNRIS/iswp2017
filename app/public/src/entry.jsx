b1bdd3112a7976a2482767ee87526b78a8629938
James Seppi <james.seppi@gmail.com>
2015-10-19 12:58:27 -0500

    include babel's polyfill

--- app/public/src/entry.jsx
+++ app/public/src/entry.jsx
@@ -2,6 +2,9 @@
  * Interactive Texas State Water Plan 2017
  * TNRIS | TWDB
  */
+
+import 'babel-core/polyfill';
+
 import React from 'react';
 import Router from 'react-router';
 import routes from './routes';
