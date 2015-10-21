a46e6d5be40c6ba7204106731d728b0783fdf7a3
James Seppi <james.seppi@gmail.com>
2015-10-21 15:11:41 -0500

    setup for statewide view

---
+++ app/public/src/components/StatewideView.jsx
@@ -0,0 +1,98 @@
+
+import React from 'react';
+import {State} from 'react-router';
+import Helmet from 'react-helmet';
+
+import constants from '../constants';
+import PlaceDataActions from '../actions/PlaceDataActions';
+import PlaceDataStore from '../stores/PlaceDataStore';
+import PlaceMap from './maps/PlaceMap';
+import PlaceSummary from './PlaceSummary';
+// import ThemeTotalsByDecadeChart from './charts/ThemeTotalsByDecadeChart';
+// import ThemeTypesByDecadeChart from './charts/ThemeTypesByDecadeChart';
+// import DataByTypeCharts from './charts/DataByTypeCharts';
+// import ThemeMaps from './maps/ThemeMaps';
+// import DataTable from './DataTable';
+
+export default React.createClass({
+  propTypes: {
+    params: React.PropTypes.shape({
+      type: React.PropTypes.string.isRequired,
+      typeId: React.PropTypes.string
+    }).isRequired
+  },
+
+  mixins: [State],
+
+  getInitialState() {
+    return PlaceDataStore.getState();
+  },
+
+  componentDidMount() {
+    PlaceDataStore.listen(this.onChange);
+
+    this.fetchPlaceData();
+  },
+
+  componentWillReceiveProps() {
+    // Route params are in this.props, and when route changes the data
+    // need to be fetched again
+    this.fetchPlaceData();
+  },
+
+  componentWillUnmount() {
+    PlaceDataStore.unlisten(this.onChange);
+  },
+
+  onChange(state) {
+    this.setState(state);
+  },
+
+  fetchPlaceData() {
+    const params = this.getParams();
+    PlaceDataActions.fetchPlaceData({
+      type: params.type, typeId: params.typeId
+    });
+  },
+
+  render() {
+    const params = this.getParams();
+    const placeData = this.state.placeData;
+
+    // TODO: REMOVE: temporary view switching by picking random region
+    const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
+    const sampleNotVal = (arr, val) => {
+      let sampleVal = '';
+      do {
+        sampleVal = sample(arr);
+      } while (sampleVal === val)
+      return sampleVal;
+    };
+    const randRegion = sampleNotVal(constants.REGIONS, params.typeId);
+    // end TODO
+
+    return (
+      <div className="statewide-view">
+        <Helmet title="Statewide" />
+        <section className="main-content">
+          <div className="place-view-top">
+            <PlaceMap className="place-map"
+              type={params.type}
+              typeId={params.typeId}
+              placeData={placeData} />
+            <div className="place-summary-wrapper wrapper">
+              <PlaceSummary
+                type={params.type}
+                typeId={params.typeId}
+                placeData={placeData} />
+            </div>
+          </div>
+          <div className="container">
+
+          </div>
+        </section>
+      </div>
+    );
+  }
+
+});
--- app/public/src/routes.es
+++ app/public/src/routes.es
@@ -5,10 +5,12 @@ import {Route, Redirect, NotFoundRoute} from 'react-router';
 import App from './App';
 import NotFound from './components/NotFound';
 import PlaceView from './components/PlaceView';
+import StatewideView from './components/StatewideView';
 
 //TODO: Remove temporary redirect from "/"
 export default (
   <Route handler={App}>
+    <Route name="stateview" path="statewide" handler={StatewideView}/>
     <Route name="placeview" path=":type/:typeId" handler={PlaceView}/>
     <Redirect from="/" to="/region/K" />
     <NotFoundRoute handler={NotFound}/>
