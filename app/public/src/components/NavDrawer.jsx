9e4c31c1c248b0cfa485be8abe0f63ae2a950314
James Seppi <james.seppi@gmail.com>
2015-11-05 10:20:51 -0600

    drawer styling

--- app/public/src/components/NavDrawer.jsx
+++ app/public/src/components/NavDrawer.jsx
@@ -1,15 +1,19 @@
 
 import React from 'react';
+import {Link} from 'react-router';
 import Drawer from 'react-motion-drawer';
 
 export default React.createClass({
   render() {
     return (
      <Drawer {...this.props} className="nav-drawer">
-        <ul>
-          <li>Home</li>
+        <ul className="main-nav-list">
+          <li>
+            <Link to={`/statewide`} activeClassName={'active'}>Statewide</Link>
+          </li>
           <li>Regions</li>
           <li>Counties</li>
+          <li>Entities</li>
           <li>Water Use Type</li>
         </ul>
       </Drawer>
