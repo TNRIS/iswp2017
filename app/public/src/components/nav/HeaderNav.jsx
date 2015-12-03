98978290c0fe942b882ec8e03dce3ded8aff93cd
James Seppi <james.seppi@gmail.com>
2015-12-03 12:35:11 -0600

    nicer styling for nav header and toggle

--- app/public/src/components/nav/HeaderNav.jsx
+++ app/public/src/components/nav/HeaderNav.jsx
@@ -2,21 +2,15 @@
 import React from 'react';
 import Select from 'react-select';
 
-// TODO: delete svg if removing
-// import MenuIcon from '../../static/img/menu.svg';
-
 export default React.createClass({
   getInitialState() {
     return {};
-  },
-
-  toggleNav() {
-    this.setState({isNavOpen: !this.state.isNavOpen});
   },
   
   changeNavCategory(e) {
     console.log(e.target.value);
   },
+
   // logChange(val) {
   //   console.log("Selected: " + val);
   // },
@@ -29,15 +23,17 @@ export default React.createClass({
     //<Select className="nav-category-select" options={options} onChange={this.logChange} />
 
     return (
-      <div className="header-nav wrapper">
-        <label htmlFor="nav-category-select">View data for</label>
-        <select onChange={this.changeNavCategory} className="nav-category-select" id="nav-category-select">
-          <option value="statewide">Statewide</option>
-          <option value="region">Region</option>
-          <option value="county">County</option>
-          <option value="entity">Water User Group</option>
-        </select>
+      <div className="header-nav">
+        <div className="wrapper">
+          <label htmlFor="nav-category-select">View data for</label>
+          <select onChange={this.changeNavCategory} className="nav-category-select" id="nav-category-select">
+            <option value="statewide">Statewide</option>
+            <option value="region">Region</option>
+            <option value="county">County</option>
+            <option value="entity">Water User Group</option>
+          </select>
 
+        </div>
       </div>
     );
   }
