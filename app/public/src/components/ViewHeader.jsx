e179732a6ba9f0430ae4bb5fb63fcd7c1c062ad7
James Seppi <james.seppi@gmail.com>
2016-01-21 14:29:33 -0600

    improve header, closes #131. thx @christoball

--- app/public/src/components/ViewHeader.jsx
+++ app/public/src/components/ViewHeader.jsx
@@ -21,8 +21,10 @@ export default React.createClass({
     return (
       <header className="header">
         <div className="header-title wrapper">
-
-          <h1><a href="/" title="Home">DRAFT 2017 Texas State Water Plan</a></h1>
+          <h1>
+            <a href="/" title="Home"><span className="first-line">DRAFT 2017</span><br/>Texas State Water Plan</a>
+          </h1>
+          <span className="by-the show-medium">by the</span>
           <a href="http://www.twdb.texas.gov" title="Texas Water Development Board">
             <img src="/public/static/img/twdb-logo-dark-bkgd-w200px-3c.png" className="logo" />
           </a>
