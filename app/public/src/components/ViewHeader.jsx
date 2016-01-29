9dfd71e770d025a802cebe13e5a336f42dd3bdaa
James Seppi <james.seppi@gmail.com>
2016-01-29 11:51:08 -0600

    header bottom border

--- app/public/src/components/ViewHeader.jsx
+++ app/public/src/components/ViewHeader.jsx
@@ -8,18 +8,20 @@ export default React.createClass({
   render() {
     return (
       <header className="header">
-        <div className="header-title wrapper">
-          <h1>
-            <a href="/" title="Home"><span className="first-line">DRAFT 2017</span><br/>Texas State Water Plan</a>
-          </h1>
-          <span className="by-the show-medium">by the</span>
-          <a href="http://www.twdb.texas.gov" title="Texas Water Development Board">
-            <img src="/public/static/img/twdb-logo-dark-bkgd-w200px-3c.png" className="logo" />
-          </a>
+        <div className="header-title-container">
+          <div className="header-title wrapper">
+            <h1>
+              <a href="/" title="Home"><span className="first-line">DRAFT 2017</span><br/>Texas State Water Plan</a>
+            </h1>
+            <span className="by-the show-medium">by the</span>
+            <a href="http://www.twdb.texas.gov" title="Texas Water Development Board">
+              <img src="/public/static/img/twdb-logo-dark-bkgd-w200px-3c.png" className="logo" />
+            </a>
 
-          <div className="nav-container">
-            <a className="button button-home" title="Home" href="/"><HomeIcon/></a>
-            <a className="button" href="/about" title="About">About</a>
+            <div className="nav-container">
+              <a className="button button-home" title="Home" href="/"><HomeIcon/></a>
+              <a className="button" href="/about" title="About">About</a>
+            </div>
           </div>
         </div>
         <HeaderNav />
