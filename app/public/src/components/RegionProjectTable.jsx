dc7b756ddcae5fe4a356d86d2d84712d45c7d9de
James Seppi <james.seppi@gmail.com>
2016-03-01 15:24:50 -0600

    add filter to projects-table, closes #181

--- app/public/src/components/RegionProjectTable.jsx
+++ app/public/src/components/RegionProjectTable.jsx
@@ -2,6 +2,7 @@
 import R from 'ramda';
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import LinkedStateMixin from 'react-addons-linked-state-mixin';
 import {Table, Tr, Td} from 'reactable';
 // import ToggleDisplay from 'react-toggle-display';
 import format from 'format-number';
@@ -13,23 +14,32 @@ export default React.createClass({
     projectData: PropTypes.ProjectData.isRequired
   },
 
-  mixins: [PureRenderMixin],
+  mixins: [LinkedStateMixin, PureRenderMixin],
+
+  getInitialState() {
+    return {};
+  },
 
   render() {
     const projectData = this.props.projectData;
     const totalCost = R.sum(R.pluck('CapitalCost', projectData));
 
     return (
-      <div>
+      <div className="recommended-projects-container">
         <h4>Recommended Projects</h4>
         <p>
           Total capital cost of recommended projects in this region: <strong>${format()(totalCost)}</strong>.
         </p>
+        <input type="text" placeholder="Type to filter table" className="table-filter"
+          valueLink={this.linkState('filter')} />
         <div className="projects-table-container">
           <Table className="table-condensed u-full-width projects-table"
             sortable
             itemsPerPage={10}
             pageButtonLimit={5}
+            filterable={['Project', 'Decade Online', 'Sponsor']}
+            hideFilterInput
+            filterBy={this.state.filter}
             defaultSort={{column: 'Project', direction: 'asc'}}>
             {
               projectData.map((d) => {
