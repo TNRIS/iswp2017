edcf7e5d7a3ca6b2616c06548aadf74ed3542bc0
James Seppi <james.seppi@gmail.com>
2016-03-01 15:04:03 -0600

    project-table styling

--- app/public/src/components/RegionProjectTable.jsx
+++ app/public/src/components/RegionProjectTable.jsx
@@ -20,37 +20,39 @@ export default React.createClass({
     const totalCost = R.sum(R.pluck('CapitalCost', projectData));
 
     return (
-      <div className="project-list-container">
+      <div>
         <h4>Recommended Projects</h4>
         <p>
-          Total capital cost of recommended projects: <strong>${format()(totalCost)}</strong>.
+          Total capital cost of recommended projects in this region: <strong>${format()(totalCost)}</strong>.
         </p>
-        <Table className="table-condensed u-full-width projects-table"
-          sortable
-          itemsPerPage={10}
-          pageButtonLimit={5}
-          defaultSort={{column: 'Project', direction: 'asc'}}>
-          {
-            projectData.map((d) => {
-              return (
-                <Tr key={d.WMSProjectId}>
-                  <Td column="Project" value={d.ProjectName}>
-                    {d.ProjectName}
-                  </Td>
-                  <Td column="Decade Online" value={d.OnlineDecade}>
-                    {d.OnlineDecade}
-                  </Td>
-                  <Td column="Sponsor" value={d.ProjectSponsors}>
-                    {d.ProjectSponsors}
-                  </Td>
-                  <Td column="Capital Cost" value={d.CapitalCost}>
-                    {`\$${format()(d.CapitalCost)}`}
-                  </Td>
-                </Tr>
-              );
-            })
-          }
-        </Table>
+        <div className="projects-table-container">
+          <Table className="table-condensed u-full-width projects-table"
+            sortable
+            itemsPerPage={10}
+            pageButtonLimit={5}
+            defaultSort={{column: 'Project', direction: 'asc'}}>
+            {
+              projectData.map((d) => {
+                return (
+                  <Tr key={d.WMSProjectId}>
+                    <Td column="Project" value={d.ProjectName}>
+                      {d.ProjectName}
+                    </Td>
+                    <Td column="Decade Online" value={d.OnlineDecade}>
+                      {d.OnlineDecade}
+                    </Td>
+                    <Td column="Sponsor" value={d.ProjectSponsors}>
+                      {d.ProjectSponsors}
+                    </Td>
+                    <Td column="Capital Cost" value={d.CapitalCost}>
+                      {`\$${format()(d.CapitalCost)}`}
+                    </Td>
+                  </Tr>
+                );
+              })
+            }
+          </Table>
+        </div>
       </div>
     );
   }
