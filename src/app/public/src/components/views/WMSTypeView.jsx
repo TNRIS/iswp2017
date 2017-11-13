fa2c9dc8a84c171e59838d1bfc27dc8d51024fc4
David Hickman <davidhickman13@gmail.com>
2017-11-13 09:49:37 -0600

    basic page layout

--- src/app/public/src/components/views/WMSTypeView.jsx
+++ src/app/public/src/components/views/WMSTypeView.jsx
@@ -14,6 +14,8 @@ import ViewStateStore from '../../stores/ViewStateStore';
 import WmsTypeSummary from '../WmsTypeSummary';
 import ThemeMaps from '../maps/ThemeMaps';
 import ProjectTable from '../ProjectTable';
+import PlacePivotTable from '../PlacePivotTable';
+import DownloadDataLink from '../DownloadDataLink';
 
 export default class WMSTypeView extends React.Component {
     constructor(props) {
@@ -37,17 +39,17 @@ export default class WMSTypeView extends React.Component {
     }
 
     componentDidUpdate = () => {
-      //if population theme selection is hidden but it is the currently selected theme,
-      // then update the theme choice to 'needs'
-      if (this.state.hidePopulation && this.state.viewChoice.selectedTheme === 'population') {
-        DataViewChoiceActions.updateThemeChoice('strategies');
-      }
+        //if population theme selection is hidden but it is the currently selected theme,
+        // then update the theme choice to 'needs'
+        if (this.state.hidePopulation && this.state.viewChoice.selectedTheme === 'population') {
+            DataViewChoiceActions.updateThemeChoice('strategies');
+        }
     }
 
     componentWillUnmount = () => {
-      WmsTypeDataStore.unlisten(this.onViewDataChange);
-      DataViewChoiceStore.unlisten(this.onDataViewChoiceChange);
-      ViewStateStore.unlisten(this.onViewStateChange);
+        WmsTypeDataStore.unlisten(this.onViewDataChange);
+        DataViewChoiceStore.unlisten(this.onDataViewChoiceChange);
+        ViewStateStore.unlisten(this.onViewStateChange);
     }
 
     onViewDataChange = (state) => {
@@ -55,7 +57,9 @@ export default class WMSTypeView extends React.Component {
     }
 
     onViewStateChange = (storeState) => {
-        this.setState({hidePopulation: this.shouldHidePopulation(storeState.viewState)});
+        this.setState({
+            hidePopulation: this.shouldHidePopulation(storeState.viewState)
+        });
     }
 
     onDataViewChoiceChange = (state) => {
@@ -69,77 +73,96 @@ export default class WMSTypeView extends React.Component {
     shouldHidePopulation = (viewState) => {
         return viewState && viewState.id !== 'municipal';
     }
-
     render() {
-      const wmsTypeData = this.state.wmsTypeData;
-      const wmsType = this.props.match.params.wmsType;
+        console.log(this.state.wmsTypeData)
+        const wmsTypeData = this.state.wmsTypeData;
+        const wmsType = this.props.match.params.wmsType;
+        const title = titleize(wmsType);
+        const viewName = titleize(wmsType);
+        const themeKeys = this.state.hidePopulation
+            ? constants.THEMES
+            : R.prepend('population', constants.THEMES);
 
-      const title = titleize(wmsType);
-      const viewName = titleize(wmsType);
-      const themeKeys = this.state.hidePopulation ? constants.THEMES
-        : R.prepend('population', constants.THEMES);
-
-      return (
-        <div className="wms-type-view">
-          <Helmet title={title} />
-          <section>
-            <div className="view-top wms-type-view-top">
-              <div className="summary-wrapper container">
-                <WmsTypeSummary wmsTypeData={wmsTypeData} wmsType={wmsType} />
-              </div>
-            </div>
-
-            {
-              (() => {
-                if (!wmsTypeData || R.isEmpty(R.keys(wmsTypeData))) {
-                  return (
-                    <div className="container">
-                      <div className="row panel-row">
-                        <div className="twelve columns">
-                          <Spinner name="double-bounce" fadeIn='none' />
-                        </div>
-                      </div>
+        return (<div className="wms-type-view">
+            <Helmet title={title}/>
+            <section>
+                <div className="view-top usage-type-view-top">
+                    <div className="summary-wrapper container">
+                        <WmsTypeSummary wmsTypeData={wmsTypeData} wmsType={wmsType}/>
                     </div>
-                  );
-                }
-                return (
-                  <div>
-                    <div className="container">
-                      <div className="row panel-row">
-                        <div className="twelve columns">
-                          <span className="view-name">{title}</span>
-                          <ProjectTable type="entity" projectData={wmsTypeData.data.projects} />
-                        </div>
-                      </div>
-                    </div>
-                    <DataViewChoiceWrap decade={this.state.viewChoice.selectedDecade}
-                      theme={this.state.viewChoice.selectedTheme}
-                      hidePopulation={this.state.hidePopulation}>
-                      <div className="container">
-                          <div className="row panel-row">
-                            <div className="twelve columns">
-                              <span className="view-name">{title}</span>
-                              <ThemeMaps placeData={wmsTypeData} view={'wmsType'}
-                                decade={this.state.viewChoice.selectedDecade}
-                                theme={this.state.viewChoice.selectedTheme} />
+                </div>
+
+                {
+                    (() => {
+                        if (!wmsTypeData || R.isEmpty(R.keys(wmsTypeData))) {
+                            return (<div className="container">
+                                <div className="row panel-row">
+                                    <div className="twelve columns">
+                                        <Spinner name="double-bounce" fadeIn='none'/>
+                                    </div>
+                                </div>
+                            </div>);
+                        }
+                        return (<div>
+                            <div className="container">
+                                <div className="row panel-row">
+                                    <div className="twelve columns">
+                                        <span className="view-name">{title}</span>
+                                        <ProjectTable type="entity" projectData={wmsTypeData.data.projects}/>
+                                    </div>
+                                </div>
                             </div>
-                          </div>
-                      </div>
-                    </DataViewChoiceWrap>
-                  </div>
-                );
-              })()
-            }
-          </section>
-        </div>
-      );
+                            <DataViewChoiceWrap decade={this.state.viewChoice.selectedDecade} hideTheme={true} theme="strategies" hidePopulation={this.state.hidePopulation}>
+                                <div className="container">
+                                    <div className="row panel-row">
+                                        <div className="twelve columns">
+                                            <span className="view-name">{title}</span>
+                                            <ThemeMaps placeData={wmsTypeData} view={'wmsType'} decade={this.state.viewChoice.selectedDecade} theme={this.state.viewChoice.selectedTheme}/>
+                                        </div>
+                                    </div>
+                                    <div className="row panel-row">
+                                      <div className="twelve columns">
+                                        <span className="view-name">{wmsType}</span>
+                                        <PlacePivotTable viewData={wmsTypeData.data}
+                                          decade={this.state.viewChoice.selectedDecade}
+                                          theme={this.state.viewChoice.selectedTheme} />
+                                        <h5>Download Data</h5>
+                                        <ul>
+                                          {
+                                            ['strategies'].map((theme) => {
+                                              if (R.isEmpty(wmsTypeData)) {
+                                                return (
+                                                  <li key={`download-${theme}`}>
+                                                    No {constants.THEME_TITLES[theme]} data exists for {wmsType} Usage Type
+                                                  </li>
+                                                );
+                                              }
+                                              return (
+                                                <li key={`download-${theme}`}>
+                                                  <DownloadDataLink
+                                                    type="wms"
+                                                    typeId={wmsType}
+                                                    theme={theme}
+                                                    viewName={wmsType} />
+                                                </li>
+                                              );
+                                            })
+                                          }
+                                        </ul>
+                                      </div>
+                                    </div>
+                                </div>
+                            </DataViewChoiceWrap>
+                        </div>);
+                    })()
+                }
+            </section>
+        </div>);
     }
 }
 
 WMSTypeView.propTypes = {
     match: PropTypes.shape({
-        params: PropTypes.shape({
-            wmsType: PropTypes.string.isRequired
-        }).isRequired
+        params: PropTypes.shape({wmsType: PropTypes.string.isRequired}).isRequired
     })
 }
